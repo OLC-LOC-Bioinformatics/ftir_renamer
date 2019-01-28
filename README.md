97778518e8aa03d45b86d213b98ac0250cecd545
Adam Koziol <adam.koziol@inspection.gc.ca>
2019-01-28 10:27:41 -0500

    Updated to Python 3.6. Reorganized to be compatible with pip packaging

--- README.md
+++ README.md
@@ -8,30 +8,19 @@ Renames files from the FTIR based on strict naming scheme
 
 From the command line in the folder in which you would like to install the program:
 
-`git clone https://github.com/adamkoziol/ftir_renamer.git`
-
-`cd ftir_renamer`
-
-`python setup.py install`
-
-Note: this final command may need to be run with sudo 
-
-Ideally add renamer.py to your path. If you don't know how, that's fine. Just run the merger.py script from within 
-the ftir_renamer folder
+`pip install ftir_renamer`
 
 ### Requirements
 - Unix-like environment
 - Spreadsheet of FTIR-related metadata
 - The .FTIR output files corresponding to the FTIR IDs in the spreadsheet
 
-### Arguments
+### Required Arguments
 
-path
-The path of the folder containing the the spreadsheet of FTIR metadata
 -s
 The path of the folder containing the FTIR output files
 -f 
-The name spreadsheet containing the FTIR metadata
+Absolute path to the spreadsheet containing the FTIR metadata
 -o
 The absolute path of the folder to store the renamed files
 
@@ -39,27 +28,19 @@ The absolute path of the folder to store the renamed files
 
 ##### Example command to run the script
 
-`renamer.py /nas0/bio_requests/8782 -f FTIR.xlsx -s /nas0/bio_requests/8782/files`
-    
-    - renamer.py is in $PATH
-    - path argument: /nas0/bio_requests/8782
-    - spreadsheet of FTIR metadata: FTIR.xlsx
-    - output folder in which renamed files are stored: /nas0/bio_requests/8782/files
+`renamer.py -f /path/to/FTIR.xlsx -s /path/to/files -o /path/to/outputs`
     
 ##### Usage
 
 ```
-usage: renamer.py [-h] -s SEQUENCEPATH -f FILENAME [-o OUTPUTPATH] path
+usage: renamer.py [-h] -s SEQUENCEPATH -f FILENAME -o OUTPUTPATH
 
 Rename files for FTIR experiments using strict naming scheme
 
-positional arguments:
-  path                  Specify input directory
-
-optional arguments:
   -h, --help            show this help message and exit
+required arguments:
   -s SEQUENCEPATH, --sequencepath SEQUENCEPATH
-                        Path of .fastq(.gz) files to process.
+                        Path of .spc files to process.
   -f FILENAME, --filename FILENAME
                         Name of .xls(x) file with renaming information. Must
                         conform to agreed upon format (see README for
--- ftir/renamer.py
+++ ftir/renamer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-import time
+from accessoryFunctions.accessoryFunctions import make_path, MetadataObject, SetupLogging
+from argparse import ArgumentParser
+from glob import glob
+import logging
 import pandas
-from accessoryFunctions.accessoryFunctions import *
+import shutil
+import time
+import os
 __author__ = 'adamkoziol'
 
 
@@ -11,12 +16,13 @@ class Renamer(object):
         """
         Parses input excel file, and creates objects with headers as keys, and cell data as values for each row
         """
+        logging.info('Loading excel file')
         # A dictionary to store the parsed excel file in a more readable format
         nesteddictionary = dict()
         # Use pandas to read in the excel file, and subsequently convert the pandas data frame to a dictionary
         # (.to_dict()). Only read the first fourteen columns (parse_cols=range(14)), as later columns are not
         # relevant to this script
-        dictionary = pandas.read_excel(self.file, parse_cols=range(14)).to_dict()
+        dictionary = pandas.read_excel(self.file, usecols=range(14)).to_dict()
         # Iterate through the dictionary - each header from the excel file
         for header in dictionary:
             # Sample is the primary key, and value is the value of the cell for that primary key + header combination
@@ -42,9 +48,9 @@ class Renamer(object):
                 try:
                     # Create each attribute - use the header (in lowercase, and spaces removed) as the attribute name,
                     # and the value as the attribute value
-                    setattr(metadata, header.replace(' ', '').encode().lower(), str(value).encode())
-                except AttributeError:
-                    setattr(metadata, header.replace(' ', '').encode().lower(), value)
+                    setattr(metadata, str(header).replace(' ', '').lower(), str(value))
+                except TypeError:
+                    setattr(metadata, str(header).replace(' ', '').lower(), value)
             # Append the object to the list of objects
             self.metadata.append(metadata)
         # Run the filer method
@@ -54,10 +60,9 @@ class Renamer(object):
         """
         Match the files to the spreadsheet. Rename and copy the files as required.
         """
-        from glob import glob
-        import shutil
         # Create the output path as required
         make_path(self.outputpath)
+        logging.info('Renaming files')
         for sample in self.metadata:
             # Use the FTIR id and replicate from the spreadsheet as part of the pattern to match to find the
             # appropriate files e.g. FTIR0018 replicate 3 will match self.sequencepath/FTIR0018-3*
@@ -87,6 +92,7 @@ class Renamer(object):
                 # GP_Bacillus_nan_TSB_AE_CFIA_FTIR0010_C2_2017_April_20_01.spc becomes
                 # GP_Bacillus_TSB_AE_CFIA_FTIR0010_C2_2017_April_20_01.spc
                 if sample.species == 'nan':
+                    logging.critical((sample.ftirid, sample.species))
                     sample.renamedfile = sample.renamedfile.replace('nan_', '')
                 # The output file will be the the renamed file in the output path
                 sample.outputfile = os.path.join(self.outputpath, sample.renamedfile)
@@ -95,50 +101,47 @@ class Renamer(object):
                     shutil.copyfile(sample.originalfile, sample.outputfile)
             # Print a message warning that certain files specified in the spreadsheet were not found in the file path
             except IndexError:
-                printtime('Missing file for {}'.format(sample.ftirid), self.start)
+                logging.warning('Missing file for {sid}'.format(sid=sample.ftirid))
 
     def __init__(self, args):
         """
         :param args: object of arguments
         """
+        SetupLogging()
         # Define variables based on supplied arguments
-        self.path = os.path.join(args.path, '')
-        assert os.path.isdir(self.path), u'Supplied path is not a valid directory {0!r:s}'.format(self.path)
-        self.sequencepath = os.path.join(args.sequencepath, '')
-        assert os.path.isdir(self.sequencepath), u'Supplied sequence path is not a valid directory {0!r:s}'\
+        self.path = os.path.join(args.path)
+        assert os.path.isdir(self.path), 'Supplied path is not a valid directory {0!r:s}'.format(self.path)
+        self.sequencepath = os.path.join(args.sequencepath)
+        assert os.path.isdir(self.sequencepath), 'Supplied sequence path is not a valid directory {0!r:s}'\
             .format(self.sequencepath)
         self.file = os.path.join(self.path, args.filename)
         self.start = args.start
-        assert os.path.isfile(self.file), u'Cannot find the supplied Excel file ({0!r:s}) with the file information. ' \
-                                          u'Please ensure that this file is in the path, and there\'s no spelling ' \
-                                          u'mistakes'.format(self.file)
+        assert os.path.isfile(self.file), 'Cannot find the supplied Excel file ({0!r:s}) with the file information. ' \
+                                          'Please ensure that this file is in the path, and there\'s no spelling ' \
+                                          'mistakes'.format(self.file)
         # If the output path is not provided, use self.path/renamedfiles as the path
         try:
             self.outputpath = os.path.join(args.outputpath, '')
 
         except AttributeError:
-            self.outputpath = os.path.join(self.path, 'renamedfiles', '')
+            self.outputpath = os.path.join(self.path, 'renamedfiles')
         # Create class variable
         self.metadata = list()
-        # Parse the excel sheet
-        self.excelparse()
+
 
 if __name__ == '__main__':
-    # Argument parser for user-inputted values, and a nifty help menu
-    from argparse import ArgumentParser
     # Parser for arguments
     parser = ArgumentParser(description='Rename files for FTIR experiments using strict naming scheme')
-    parser.add_argument('path',
-                        help='Specify input directory')
     parser.add_argument('-s', '--sequencepath',
                         required=True,
-                        help='Path of .fastq(.gz) files to process.')
+                        help='Path of .spc files')
     parser.add_argument('-f', '--filename',
                         required=True,
-                        help='Name of .xls(x) file with renaming information. Must conform to agreed upon format '
-                             '(see README for additional information). This file must be in the path.')
+                        help='Absolute path to .xls(x) file with renaming information. Must conform to required format '
+                             '(see README for additional information)')
     parser.add_argument('-o', '--outputpath',
-                        help='Optionally specify the folder in which the renamed files are to be stored. Provide the'
+                        required=True,
+                        help='Specify the folder in which the renamed files are to be stored. Provide the'
                              'full path e.g. /path/to/output/files')
     # Get the arguments into an object
     arguments = parser.parse_args()
@@ -146,7 +149,8 @@ if __name__ == '__main__':
     arguments.start = time.time()
 
     # Run the script
-    Renamer(arguments)
+    renamer = Renamer(arguments)
+    renamer.excelparse()
 
     # Print a bold, green exit statement
     print('\033[92m' + '\033[1m' + "\nElapsed Time: %0.2f seconds" % (time.time() - arguments.start) + '\033[0m')
